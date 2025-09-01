bd9896fa05c8ba1a01651c7ddaf08d09ff2d913d
gsegria <pinghsu1025@gmail.com>
2025-09-01 17:22:05 +0800

    auto

---
+++ workspace/demo_myProject/audio_demo_pc/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : audio_demo_pc
+// Author      : Ping Hsu
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!audio_demo_pc!!!" << endl;
+	return 0;
+}
